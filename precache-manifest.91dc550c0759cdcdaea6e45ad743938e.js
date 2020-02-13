ed265eb5119f1ecfb21a80ad8bcffe14f013a804
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-02-13 14:39:05 +0100

    Updates

---
+++ precache-manifest.91dc550c0759cdcdaea6e45ad743938e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe32e5ba0976f191e32e1c5cf4a504f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "71d440a51acf20a2f330",
+    "url": "/todo-list/static/css/main.a5a4abd5.chunk.css"
+  },
+  {
+    "revision": "e320415ac2ccaacac210",
+    "url": "/todo-list/static/js/2.e28f204a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-list/static/js/2.e28f204a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d440a51acf20a2f330",
+    "url": "/todo-list/static/js/main.fa19deb3.chunk.js"
+  },
+  {
+    "revision": "4882d337b710f0cba9ae",
+    "url": "/todo-list/static/js/runtime-main.273644b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
